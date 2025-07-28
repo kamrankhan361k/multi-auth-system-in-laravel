2f7752a89226f0a801175d10e47527cba0f51829
unknown <kamrankhan3.61k@gmail.com>
2025-07-28 10:15:01 +0500

    creating tabels code migrations and run

--- database/migrations/2025_07_28_045439_create_students_table.php
+++ database/migrations/2025_07_28_045439_create_students_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+              $table->id();
+    $table->string('name');
+    $table->string('email')->unique();
+    $table->timestamp('email_verified_at')->nullable();
+    $table->string('password');
+    $table->rememberToken();
+    $table->timestamps();
         });
     }
 
